4ca204ed3da1578fd8c59a8ba3b5b9c308194d55
Administrator <709810345@qq.com>
2017-11-10 15:08:06 +0800

    add feature-1 on feature-1 branch

--- app/src/main/java/com/example/administrator/mygit/MainActivity.java
+++ app/src/main/java/com/example/administrator/mygit/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.administrator.mygit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    /**
-     * 测试
-     * @param savedInstanceState
-     */
+
+    Button btn_feature_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature1(View view) {
+        if (btn_feature_1.getText().toString().equals("功能1")) {
+            btn_feature_1.setText(R.string.feature_1_dis);
+        } else {
+            btn_feature_1.setText(R.string.feature_1);
+        }
     }
 }

2dd5b9a38e7d26fcd4c81f0440f13a54667494f6
Administrator <709810345@qq.com>
2017-11-10 15:20:07 +0800

    add feature-2 on branch feature-2

--- app/src/main/java/com/example/administrator/mygit/MainActivity.java
+++ app/src/main/java/com/example/administrator/mygit/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button btn_feature_1;
+    Button btn_feature_2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,18 @@ public class MainActivity extends AppCompatActivity {
             btn_feature_1.setText(R.string.feature_1_dis);
         } else {
             btn_feature_1.setText(R.string.feature_1);
+        }
+    }
+    /**
+     * feature-2
+     * display HelloAndroid
+     * @param view
+     */
+    public void feature2(View view) {
+        if (btn_feature_2.getText().toString().equals("功能2")) {
+            btn_feature_2.setText(R.string.feature_2_dis);
+        } else {
+            btn_feature_2.setText(R.string.feature_2);
         }
     }
 }
